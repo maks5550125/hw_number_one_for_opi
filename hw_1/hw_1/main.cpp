8af9d4b9dbd38112f992c383491bbba4303711c0
Vldmrkv <finditproduction@gmail.com>
2023-04-01 13:00:13 +0300

    Added prototypes of functions

--- hw_1/hw_1/main.cpp
+++ hw_1/hw_1/main.cpp
@@ -2,6 +2,18 @@
 #include<limits.h>
 #include<ctime>
 
+using namespace std;
+
+const int MatrixSize{ 16 };
+
+void CreateMatrix(int(&matrix)[MatrixSize][MatrixSize]);
+void PrintMatrix(int(&matrix)[MatrixSize][MatrixSize]);
+void FillMaximalIndexes(int(&matrix)[MatrixSize][MatrixSize], int(&maxIndexElements)[MatrixSize]);
+void CalculateMultiplicationAfterMaxElementInRow(int(&matrix)[MatrixSize][MatrixSize], int(&maxIndexElements)[MatrixSize], int(&sumOfRows)[MatrixSize]);
+void PrintMultiplicationAfterMaxElementInRow(int(&sumOfRows)[MatrixSize]);
+bool CheckingRowsForSymmetry(int matrix[][MatrixSize], const int MatrixSize);
+int CalculateSumOfBlackMatrixElements(int matrix[][MatrixSize], const int MatrixSize);
+
 int main() {
 
 	return 0;
