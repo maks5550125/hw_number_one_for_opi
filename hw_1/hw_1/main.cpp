ae86aa2f291ee76d52caf9dd34702c5998f0d137
Valyavkin Maxim <valyavkin99@mail.ru>
2023-04-01 13:04:26 +0300

    Added CreateMatrix function

--- hw_1/hw_1/main.cpp
+++ hw_1/hw_1/main.cpp
@@ -18,3 +18,11 @@ int main() {
 
 	return 0;
 }
+
+void CreateMatrix(int (&matrix)[MatrixSize][MatrixSize]) {
+    for (int i = 0; i < MatrixSize; i++) {
+        for (int j = 0; j < MatrixSize; j++) {
+            matrix[i][j] = rand() % 19 + (-9);
+        }
+    }
+}
